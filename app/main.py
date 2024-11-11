61a8c831a251d93f05b08f6fae789343a499a057
seancze <60085582+seancze@users.noreply.github.com>
2024-11-11 14:16:47 +0800

    feat: update endpoint to calculate vector embeddings before uploading questions to db

--- app/main.py
+++ app/main.py
@@ -5,45 +5,52 @@ from fastapi.responses import JSONResponse
 from pymongo import MongoClient
 from datetime import datetime, timezone
 from app.models import QuestionData
-from app.utils.format_utils import clean_meta_info, convert_exam_type
+from app.utils.format_utils import convert_exam_type
+from app.create_vector_embeddings import get_embedding
+from openai import OpenAI
 
 load_dotenv()
 
-# Initialize FastAPI app
+# initialise FastAPI app
 app = FastAPI()
 
-# Connect to MongoDB
-client = MongoClient(os.environ.get("MONGODB_URI"))
-db = client["exam_db"]
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# connect to MongoDB
+mongo_client = MongoClient(os.environ.get("MONGODB_URI"))
+db = mongo_client["exam_db"]
 question_collection = db["question"]
 
 
-# Endpoint to accept JSON data and store in MongoDB
+# endpoint to accept JSON data and store in MongoDB
 @app.post("/upload_questions")
 async def upload_questions(request_obj: QuestionData):
     try:
-        # Clean meta_info
         meta_info = request_obj.meta_info.model_dump()
 
-        # Convert exam_type to enum value
+        # convert exam_type to enum value
         exam_type = convert_exam_type(meta_info.get("exam_type", ""))
 
-        # Iterate over each question and insert into MongoDB
+        # iterate over each question and insert into MongoDB
         for idx, question_item in enumerate(request_obj.questions, start=1):
-            # Clean question data
+            # clean question data
             question_body = question_item.question.strip()
             category = question_item.category.strip()
             question_type = question_item.question_type.strip()
             question_number = int(question_item.question_number.strip())
             question_part = question_item.question_part.strip()
 
-            # Prepare document to insert
+            # get embedding for question body
+            question_body_embedding = get_embedding(openai_client, question_body)
+
+            # prepare document to insert
             question_document = {
                 "page_start": question_item.page_start,
                 "page_end": question_item.page_end,
                 "question_number": question_number,
                 "question_part": question_part,
                 "question_body": question_body,
+                "question_body_embedding": question_body_embedding,
                 "topic": category,
                 "sub_topic": question_type,
                 "answer_body": "",
@@ -58,7 +65,7 @@ async def upload_questions(request_obj: QuestionData):
                 "updated_utc": datetime.now(timezone.utc),
             }
 
-            # Insert document into MongoDB
+            # insert document into MongoDB
             question_collection.insert_one(question_document)
 
         return JSONResponse(
